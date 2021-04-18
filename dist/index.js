2a11d11ce6b5c9afc0b6eb711935b334c046b752
darkdarcool <darkdarcool@gmail.com>
2021-04-18 15:15:25 +0000

    Automatically built unbuilt typescript

--- dist/index.js
+++ dist/index.js
@@ -1,40 +1,10 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getpresent = exports.sayHello = void 0;
 function sayHello(name) {
     return "Hello " + name + "!";
 }
 exports.sayHello = sayHello;
-var readline = __importStar(require("readline"));
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-rl.question('What is is passcode? ', function (answer) {
-    answer = answer.toLowerCase();
-    if (answer != "04308")
-        console.log("EEEEE");
-    rl.close();
-});
 var present = {
     "one": "Enter The Gungeon"
 };
