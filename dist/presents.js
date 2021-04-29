ba5e24b6c7d74aade4b4a5ee9b9a37d12f6133f0
darkdarcool <darkdarcool@gmail.com>
2021-04-29 17:53:49 +0000

    Automatically built unbuilt typescript

--- dist/presents.js
+++ dist/presents.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.present = void 0;
 exports.present = {
-    "one": "Enter The Gungeon",
-    "two": "Hyrule Warriors | Age of Calamity",
-    "three": "Phone",
-    "four": "Laptop",
-    "five": "Valorant"
+    one: "Enter The Gungeon",
+    two: "Hyrule Warriors | Age of Calamity",
+    three: "Phone",
+    four: "Laptop",
+    five: "Valorant",
+    six: "Zelda Breath of The Wild DLC's(there are two)",
 };
